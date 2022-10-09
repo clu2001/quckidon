a9a60275c236513a4eeb77e3de657201ba2ce402
Calla Lu <46143943+clu2001@users.noreply.github.com>
2022-10-09 14:36:17 -0400

    created character class

---
+++ frontend/src/Components/Character.tsx
@@ -0,0 +1,25 @@
+import React from "react"; 
+
+class Character {
+    name: string; 
+    age: string; 
+    gender: string; 
+    features: string; 
+    bio: string; 
+
+    constructor (
+        name: string, 
+        age: string, 
+        gender: string, 
+        features: string, 
+        bio: string, 
+    ) {
+        this.name = name; 
+        this.age = age; 
+        this.gender = gender; 
+        this.features = features; 
+        this.bio = bio; 
+    }; 
+    
+
+}
